c20525d3be6f3bbf46794ed64533a59dbbabd435
Alex <alekgutu@gmail.com>
2021-02-16 23:06:59 +0300

    created 3 new files

---
+++ js/variables/task-3.js
@@ -0,0 +1,9 @@
+/*Объявите три переменных: hour, minute, second.
+Присвойте им следующие значения: 10, 40, 25.
+Выведите в консоль время в формате 10:40:25.*/ 
+
+let hour = 10;
+let minute = 40;
+let seconds = 25;
+console.log(`${hour}:${minute}:${seconds}`);
+
