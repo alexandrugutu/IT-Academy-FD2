293bdb6f288e318283bd21ce98baf2f681f8f8d0
alexandrugutu <78763279+alexandrugutu@users.noreply.github.com>
2021-03-08 16:52:25 +0100

    Update task-6.js

--- js/numbers/task-6.js
+++ js/numbers/task-6.js
@@ -1,24 +1,17 @@
 /* Даны переменные a и b. Проверьте, что a делится без остатка на b. 
-Если это так - выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и остаток от деления.
-let a = 1.5;
-let b = 1.8;
-if (a/b.toFixed(3))
-console.log(`Divided with remainder: ${a/b}`); else 
-console.log("Делится с остатком");   */
+Если это так - выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и остаток от деления. */
 
 let a;
 let b;
 
-function findRemainder (a, b) {
+function findRemainder(a, b) {
     let result = a / b;
     let reminder = a % b;
-    if (result == 0) {
-        console.log('Число ' + result + ' делится без остатвка');
+    if (reminder == 0) {
+        return 'делится ' + result;
     }
     else {
-        console.log('Число НЕ делится без остатвка. Остаток = ' + reminder + '');
+        return 'Делится с остатком ' + reminder;
     }
-
-    return;
 }
-findRemainder(17, 2);
+console.log(findRemainder(13, 2));
